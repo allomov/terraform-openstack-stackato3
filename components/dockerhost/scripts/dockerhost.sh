9c4edfd1f94b01a133f8a60bce9b1b684c9d6f7e
A.S. Lomoff <lomov.as@gmail.com>
2017-07-27 09:11:22 -0400

    add docker-compose

--- components/dockerhost/scripts/dockerhost.sh
+++ components/dockerhost/scripts/dockerhost.sh
@@ -6,3 +6,7 @@ sudo apt-get update
 apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
+
+sudo bash -c "curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+
